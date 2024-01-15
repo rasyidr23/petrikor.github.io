ee6c5fd74a4ba4d6e77825c5dd06ebc5a1700a00
Rasyid Ridho Ibnulmuhsin <github_rasyidr23>
2024-01-15 10:38:10 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,362 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="shortcut icon" href="assets/img/favicon.png" type="image/png">
+
+        <!--=============== REMIXICONS ===============-->
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+        
+        <!--=============== SWIPER CSS ===============-->
+        <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+
+        <!--=============== CSS ===============-->
+        <link rel="stylesheet" href="assets/css/styles.css">
+
+        <title>Petrikor - Event Organizers</title>
+        
+    </head>
+    <body>
+        <header class="header" id="header">
+            <nav class="nav container">
+                <a href="#" class="nav__logo">Petrikor</a>
+
+                <div class="nav__menu" id="nav-menu">
+                    <ul class="nav__list">
+                        <li class="nav__item">
+                            <a href="#home" class="nav__link active-link">Home</a>
+                        </li>
+                        <li class="nav__item">
+                            <a href="#about" class="nav__link">About</a>
+                        </li>
+                        <li class="nav__item">
+                            <a href="#discover" class="nav__link">Discover</a>
+                        </li>
+                        <li class="nav__item">
+                            <a href="#Review" class="nav__link">Review</a>
+                        </li>
+                    </ul>
+
+                    <div class="nav__dark">
+                        <!-- Theme change button -->
+                        <span class="change-theme-name">Dark mode</span>
+                        <i class="ri-moon-line change-theme" id="theme-button"></i>
+                    </div>
+
+                    <i class="ri-close-line nav__close" id="nav-close"></i>
+                </div>
+
+                <div class="nav__toggle" id="nav-toggle">
+                    <i class="ri-function-line"></i>
+                </div>
+            </nav>
+        </header>
+
+        <main class="main">
+            <!--==================== HOME ====================-->
+            <section class="home" id="home">
+                <div class="home__container container grid">
+                    <div class="home__data">
+                        <span class="home__data-subtitle">Event Organizers</span>
+                        <h1 class="home__data-title">Make everything <br> more <b>Beautiful</b></h1>
+                        
+
+                    </div>
+
+                    <div class="home__social">
+                        </a>
+                        <a href="https://www.instagram.com/petrikor_cerita/" target="_blank" class="home__social-link">
+                            <i class="ri-instagram-fill"></i>
+                        </a>
+                        <a href="https://api.whatsapp.com/send/?phone=6285222285998" target="_blank" class="home__social-link">
+                            <i class="ri-whatsapp-fill"></i>
+                        <a href="https://linktr.ee/petrikorpro" target="_blank" class="home__social-link">
+                            <i class="ri-links-fill"></i>    
+                        </a>
+                    </div>
+                </div>
+            </section>
+
+            <!--==================== ABOUT ====================-->
+            <section class="about section" id="about">
+                <div class="about__container container grid">
+                    <div class="about__data">
+                        <div class="about__img">
+                            <div class="about__img-overlay">
+                                <img src="assets/img/logo petrikor.png" alt="" class="about__img-one">
+                            </div>
+                        </div>
+                        <h2 class="section__title about__title"> <br> Petrikor </h2>
+                        <p class="about__description">Setiap momen dalam hidup perlu dirayakan. Petrikor membuat momen berharga menjadi cerita yang akan selalu terpatri dalam ingatan. Mulai dari ulang tahun, gathering perusahaan, pesta bersama keluarga atau teman, tunangan, sampai dengan perayaan cinta dalam pernikahan dapat kami buat dan abadikan sesuai dengan perjalananmu. Karena di sini, kami berbagi rasa dengan cerita.
+                            <br> Oleh karena itu Vameratale hadir untuk melengkapi berbagai perayaan dalam hidup. Kasih sayang perlu diungkapkan dan Vameratale adalah caranya, karena apa yang dibuat dengan hati akan sampai ke hati.
+                        </p>
+                    </div>
+
+                        <div class="about__data">
+                            <div class="about__img">
+                                <div class="about__img-overlay">
+                                    <img src="assets/img/vamera colorr.png" alt="" class="about__img-two">
+                                </div>
+                            </div>
+                            <h2 class="section__title about__title"> <br> Vameratale </h2>
+                            <p class="about__description">Setiap momen dalam hidup perlu dirayakan. Petrikor membuat momen berharga menjadi cerita yang akan selalu terpatri dalam ingatan. Mulai dari ulang tahun, gathering perusahaan, pesta bersama keluarga atau teman, tunangan, sampai dengan perayaan cinta dalam pernikahan dapat kami buat dan abadikan sesuai dengan perjalananmu. Karena di sini, kami berbagi rasa dengan cerita.
+                                <br> Oleh karena itu Vameratale hadir untuk melengkapi berbagai perayaan dalam hidup. Kasih sayang perlu diungkapkan dan Vameratale adalah caranya, karena apa yang dibuat dengan hati akan sampai ke hati.
+                            </p>
+                        </div>
+
+            </section>
+            
+            <!--==================== DISCOVER ====================-->
+            <section class="discover section" id="discover">
+                <h2 class="section__title"> Make it all worthwhile <br> by immortalizing it </h2>
+                
+                <div class="discover__container container swiper-container">
+                    <div class="swiper-wrapper">
+                        <!--==================== DISCOVER 1 ====================-->
+                        <div class="discover__card swiper-slide">
+                            <img src="assets/img/gmbr10.jpg" alt="" class="discover__img">
+                            <div class="discover__data">
+                            </div>
+                        </div>
+
+                        <!--==================== DISCOVER 2 ====================-->
+                        <div class="discover__card swiper-slide">
+                            <img src="assets/img/gmbr11.jpg" alt="" class="discover__img">
+                            <div class="discover__data">
+                            </div>
+                        </div>
+
+                        <!--==================== DISCOVER 3 ====================-->
+                        <div class="discover__card swiper-slide">
+                            <img src="assets/img/gmbr3.jpg" alt="" class="discover__img">
+                            <div class="discover__data">
+                            </div>
+                        </div>
+
+                        <!--==================== DISCOVER 4 ====================-->
+                        <div class="discover__card swiper-slide">
+                            <img src="assets/img/gmbr5.jpg" alt="" class="discover__img">
+                            <div class="discover__data">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!--==================== EXPERIENCE ====================-->
+            <section class="experience section">
+                <h2 class="section__title">With Our Experience <br> We Will Serve You</h2>
+
+                <div class="experience__container container grid">
+                    <div class="experience__content grid">
+                        <div class="experience__data">
+                            <h2 class="experience__number">2</h2>
+                            <span class="experience__description">Year <br> Experience</span>
+                        </div>
+
+                        <div class="experience__data">
+                            <h2 class="experience__number">75</h2>
+                            <span class="experience__description">Complete <br> Event</span>
+                        </div>
+
+                        <div class="experience__data">
+                            <h2 class="experience__number">100+</h2>
+                            <span class="experience__description">Customer</span>
+                        </div>
+                    </div>
+
+                    <div class="experience__img grid">
+                        <div class="experience__overlay">
+                            <img src="assets/img/logo petrikor.png" alt="" class="experience__img-one">
+                        </div>
+
+                    <div class="experience__img grid">
+                        <div class="experience__overlay">
+                            <img src="assets/img/vamera colorr.png" alt="" class="experience__img-two">
+                        </div>
+                        
+                    </div>
+                </div>
+            </section>
+
+            <!--==================== VIDEO ====================-->
+            <section class="video section">
+                <h2 class="section__title">Event Organizers</h2>
+
+                <div class="video__container container">
+                    <p class="video__description">Video</p>
+
+                    <div class="video__content">
+                        <video id="video-file">
+                            <source src="assets/video/video.mp4" type="video/mp4">
+                        </video>
+
+                        <button class="button button--flex video__button" id="video-button">
+                            <i class="ri-play-line video__button-icon" id="video-icon"></i>
+                        </button>
+                    </div>
+                </div>
+            </section>
+
+            <!--===== Review ====-->
+            <section class="review" id="Review">
+                <h2 class="review_title">Review Customers</h2>
+                <div class="review_box">
+                    <div class="comment-box">
+                        <div class="box-top">
+                            <div class="Profile">
+                                <div class="profile-image">
+                                    <img src="image/1.png">
+                                </div>
+                                <div class="Name">
+                                    <strong>Ranidi Lochana</strong>
+                                    <span>@Ranidi Lochana</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="comment">
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            </p>
+                        </div>
+                    </div>
+        
+                    <div class="comment-box">
+                        <div class="box-top">
+                            <div class="Profile">
+                                <div class="profile-image">
+                                    <img src="image/2.png">
+                                </div>
+                                <div class="Name">
+                                    <strong>Senuda Dilwan</strong>
+                                    <span>@senuda dilwan</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="comment">
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            </p>
+                        </div>
+                    </div>
+        
+                    <div class="comment-box">
+                        <div class="box-top">
+                            <div class="Profile">
+                                <div class="profile-image">
+                                    <img src="image/3.png">
+                                </div>
+                                <div class="Name">
+                                    <strong>Rumali fernando</strong>
+                                    <span>@Rumali fernando</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="comment">
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            </p>
+                        </div>
+                    </div>
+        
+                    <div class="comment-box">
+                        <div class="box-top">
+                            <div class="Profile">
+                                <div class="profile-image">
+                                    <img src="image/4.png">
+                                </div>
+                                <div class="Name">
+                                    <strong>Midinu Thiranjana</strong>
+                                    <span>@Midinu Thiranjana</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="comment">
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        
+        <!--==================== FOOTER ====================-->
+        <footer class="footer section">
+            <div class="footer__container container grid">
+                <div class="footer__content grid">
+                    <div class="footer__data">
+                        <h3 class="footer__title">Petrikor</h3>
+                        <p class="footer__description">Petrikor membuat momen <br> berharga menjadi cerita yang akan <br> selalu terpatri dalam ingatan.
+                        </p>
+                        <div>
+                            <a href="https://www.instagram.com/petrikor_cerita/" target="_blank" class="footer__social">
+                                <i class="ri-instagram-fill"></i>
+                            </a>
+                            <a href="https://www.youtube.com/@petrikorpro" target="_blank" class="footer__social">
+                                <i class="ri-youtube-fill"></i>
+                            </a>
+                        </div>
+                    </div>
+    
+                    <div class="footer__data">
+                        <h3 class="footer__subtitle">About</h3>
+                        <ul>
+                            <li class="footer__item">
+                                <a href="#about" class="footer__link">About Us</a>
+                            </li>
+                            <li class="footer__item">
+                                <a href="" class="footer__link">Features</a>
+                            </li>
+                            <li class="footer__item">
+                                <a href="" class="footer__link">New & Blog</a>
+                            </li>
+                        </ul>
+                    </div>
+    
+                    <div class="footer__data">
+                        <h3 class="footer__subtitle">Support</h3>
+                        <ul>
+                            <li class="footer__item">
+                                <a href="https://api.whatsapp.com/send/?phone=6285222285998" class="footer__link">Whatsapp</a>
+                                <i class="ri-whatsapp-fill"></i>
+                            </li>
+                            <li class="footer__item">
+                                <a href="https://linktr.ee/petrikorpro" class="footer__link">Linktree</a>
+                                <i class="ri-links-fill"></i>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="footer__rights">
+                    <p class="footer__copy">&#169; 2024 Petrikor. All rigths reserved.</p>
+                    <div class="footer__terms">
+                        <a href="#" class="footer__terms-link">Terms & Agreements</a>
+                        <a href="#" class="footer__terms-link">Privacy Policy</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+         <!--========== SCROLL UP ==========-->
+        <a href="#" class="scrollup" id="scroll-up">
+            <i class="ri-arrow-up-line scrollup__icon"></i>
+        </a>
+
+        <!--=============== SCROLL REVEAL===============-->
+        <script src="assets/js/scrollreveal.min.js"></script>
+        
+        <!--=============== SWIPER JS ===============-->
+        <script src="assets/js/swiper-bundle.min.js"></script>
+
+        <!--=============== MAIN JS ===============-->
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
